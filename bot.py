3007f4264f630541675df1a1a12353008ec478cd
Klutrem <53902318+Klutrem@users.noreply.github.com>
2021-12-02 22:32:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def write_msg(user_id, message, keyboard):
 def pen():
 	vk.method('messages.send', {'user_id':my_id, 'message':'Penek', 'random_id':random.randint(0 ,2048), 'peer_id' : 7821099})
 
-token = '58057a7288e5f10ccfc59a4e4bc553a9f2bcb31a1f91d7a1922c174b9f2fa25d6ef3468c7a07d544f0cab'
+token = ''
 vk = vk_api.VkApi(token = token)
 longpoll = VkLongPoll(vk)
 my_id = 62310117
